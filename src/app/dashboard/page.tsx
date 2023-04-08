8b845f942029cafc3937d1a44ea9da7daba503f9
Kanono Ramashamole <kanono@gmail.com>
2023-04-08 19:04:36 +0200

    First commit and project setup

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Button from '@/components/ui/Button'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div className='p-8'>
+        <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default page
