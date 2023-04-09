835770489f043bfed0e2c445d23d8c1d4fc80198
Kanono Ramashamole <kanono@gmail.com>
2023-04-09 12:38:00 +0200

    Fix  button hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import ClientOnly from '@/components/ClientOnly'
 import './globals.css'
 
 export const metadata = {
@@ -12,7 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ClientOnly>
+           {children}
+        </ClientOnly>
+       
+      </body>
     </html>
   )
 }
